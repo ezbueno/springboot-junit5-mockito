f912dc9336b286548d4eb5502835ae0b6a439c50
ezbueno <ezandrobueno@yahoo.com.br>
2021-10-27 18:03:30 -0300

    classe UserResource - findById

--- src/main/java/com/buenoezandro/api/resources/UserResource.java
+++ src/main/java/com/buenoezandro/api/resources/UserResource.java
@@ -1,18 +1,23 @@
 package com.buenoezandro.api.resources;
 
 import com.buenoezandro.api.domain.User;
+import com.buenoezandro.api.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/user")
 public class UserResource {
 
+    private final UserService userService;
+
     @GetMapping(path = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Ezandro", "ezbueno@teste.com", "123"));
+        return ResponseEntity.ok().body(this.userService.findById(id));
     }
 }

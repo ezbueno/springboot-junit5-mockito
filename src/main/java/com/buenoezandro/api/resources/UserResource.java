97a46afd8622f8c70b47ac698f1a190340bc53da
ezbueno <ezandrobueno@yahoo.com.br>
2021-10-27 20:39:15 -0300

    retorna um DTO para UserResource

--- src/main/java/com/buenoezandro/api/resources/UserResource.java
+++ src/main/java/com/buenoezandro/api/resources/UserResource.java
@@ -1,8 +1,9 @@
 package com.buenoezandro.api.resources;
 
-import com.buenoezandro.api.domain.User;
+import com.buenoezandro.api.domain.dto.UserDTO;
 import com.buenoezandro.api.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     private final UserService userService;
+    private final ModelMapper modelMapper;
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(this.userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(this.modelMapper.map(this.userService.findById(id), UserDTO.class));
     }
 }

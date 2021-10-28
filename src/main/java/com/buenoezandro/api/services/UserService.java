259ca3b0185cff54c77080bad4463324865859a5
ezbueno <ezandrobueno@yahoo.com.br>
2021-10-28 12:52:13 -0300

    endpoint findAll para User

--- src/main/java/com/buenoezandro/api/services/UserService.java
+++ src/main/java/com/buenoezandro/api/services/UserService.java
@@ -2,7 +2,10 @@ package com.buenoezandro.api.services;
 
 import com.buenoezandro.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
--- src/main/java/com/buenoezandro/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/buenoezandro/api/services/impl/UserServiceImpl.java
@@ -6,6 +6,9 @@ import com.buenoezandro.api.services.UserService;
 import com.buenoezandro.api.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,9 +16,16 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public User findById(Integer id) {
         var user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> findAll() {
+        return this.userRepository.findAll();
     }
 }
